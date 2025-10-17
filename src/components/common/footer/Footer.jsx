a92e0b79e1d441f2ca3db50b85b257fa8237cea2
ÂxtêrN <119152906+rohitYaduvanshi@users.noreply.github.com>
2025-10-17 10:05:40 +0530

    commit

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 Popertix. Designd By GorkCoder.</span>
+        <span>© 2021 Popertix. Designd By Saikotx-BBIT.</span>
       </div>
     </>
   )
